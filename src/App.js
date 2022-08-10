f42813ea6a16016b8734265dd48bcbeb9af265ef
Khoshanlo Sharip <khoshanlosharip@gmail.com>
2022-08-10 12:00:55 +0600

    localStorage by redux-persist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/layout/navigation'
-import store from './redux/store';
+import store, {persistor} from './redux/store';
 import Game from './components/gameComponents/Game';
 import Statistics from './components/statisticsComponents/statistics';
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/es/integration/react'
+
 
 
 function App() {
@@ -12,11 +14,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Provider store={store}>
-          <Navigation />
-          <Routes>
-            <Route path='/' element={<Game />} />
-            <Route path='/statistics' element={<Statistics />} />
-          </Routes>
+          <PersistGate loading={null} persistor={persistor}>
+            <Navigation />
+            <Routes>
+              <Route path='/' element={<Game />} />
+              <Route path='/statistics' element={<Statistics />} />
+            </Routes>            
+          </PersistGate>
+
         </Provider>
       </BrowserRouter>
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit"
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import cluesSlice from "../features/game/cluesSlice";
 import userReducer from "../features/game/loggedSlice";
 import starterReducer from '../features/game/starterSlice'
@@ -6,14 +19,32 @@ import historySlice from "../features/statistics/historySlice";
 import statisticSlice from "../features/statistics/statisticSlice";
 
 
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-        starter: starterReducer,
-        clues: cluesSlice,
-        statistics: statisticSlice,
-        history: historySlice
-    },
+const rootReducer = combineReducers({
+    user: userReducer,
+    starter: starterReducer,
+    clues: cluesSlice,
+    statistics: statisticSlice,
+    history: historySlice,
 })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export const persistor = persistStore(store)
 
 export default store;
